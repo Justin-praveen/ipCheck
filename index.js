f1e1f4a6ba24be2b102a0b0eb7364fe3698fa05c
justinpraveen1417 <praveenc@thetopclassentertainment.com>
2023-06-17 18:58:49 +0530

    chages done2

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 
 
-
 const app = express()
 
 app.get("/", (req, res) => {
 
-    res.send(req.socket.remoteAddress)
+    const ip =
+        req.headers['cf-connecting-ip'] ||
+        req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress || '';
+
+    res.json({
+        ip
+    })
 
 })
 
